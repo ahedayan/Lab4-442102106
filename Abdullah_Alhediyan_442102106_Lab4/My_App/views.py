6348d11d73c64caab4fabddbd40e3aa08312dfde
Abdullah Alhediyan <aahedayan@hotmail.com>
2023-04-26 23:22:56 +0300

    Add 2 html files

--- Abdullah_Alhediyan_442102106_Lab4/My_App/views.py
+++ Abdullah_Alhediyan_442102106_Lab4/My_App/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 # Create your views here.
 
 
@@ -7,12 +6,12 @@ tax_rate = 15
 
 
 def view1(request):
-    return HttpResponse("this is a site to calculate tax")
+    return render(request, "My_App/defaultPath.html")
 
 
 def view2(request, number):
     number = number * (1+(tax_rate/100))
-    return HttpResponse(number)
+    return render(request, "My_App/calculate.html", {"number": number})
 
 
 def view3(request):
